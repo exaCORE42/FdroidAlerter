c8f29937075fe6f87af11018708b6635e1cae2a5
exacore <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2021-12-26 16:56:39 -0500

    adding some stuff

--- updatereminders.py
+++ updatereminders.py
@@ -2,61 +2,85 @@ from bs4 import BeautifulSoup
 import requests
 from rfeed import *
 import datetime
+from google_play_scraper import app
 
-#set up beautifulsoup for download page
-downloadpage = requests.get("https://dolphin-emu.org/download/")
-downloadpagesoup = BeautifulSoup(downloadpage.content, 'html.parser')
 
-# find the download link on the page
-downloadpagetag = downloadpagesoup.body.find('td', "version always-ltr")
-print(downloadpagetag)
-print(str(downloadpagetag)[40:])
-for letternum in range(len(str(downloadpagetag)[40:])):
-    if str(downloadpagetag)[40:][letternum] == '"':
-        downloadlink = str("https://dolphin-emu.org" + (str(downloadpagetag)[40:int(40+letternum)]))
-        break
-
-#set up beautifulsoup for commitpage
-commitpage = requests.get(downloadlink)
-commitpagesoup = BeautifulSoup(commitpage.content, 'html.parser')
-
-# find the commit link
-commitpagetag = commitpagesoup.body.find('dd', "commithash")
-print()
-for letternum in range(len(str(commitpagetag)[32:])):
-    if str(commitpagetag)[32:][letternum] == '"':
-        commitlink = str(commitpagetag)[32:int(32+letternum)]
-        break
-
-global line1
-line1 = str("@licaon-kter The current version of Dolphin Emulator on Fdroid is outdated.")
-global line2
-line2 = str("The commit link is: " + commitlink)
-global line3
-line3 = str("The direct download link is: " + downloadlink)
-print(datetime.datetime(datetime.datetime.now().date().year, datetime.datetime.now().date().month,
-                        datetime.datetime.now().date().day, datetime.datetime.now().time().hour,
-                        datetime.datetime.now().time().minute))
-item1 = Item(
-    title = "NEW UPDATE TO DOLPHIN EMULATOR",
-    link = "https://gitlab.com/fdroid/fdroiddata/-/issues/2043",
-    description = '<p>'+ line1 +'</p><p>'+line2+'</p><p>'+line3+'</p>',
-    pubDate = datetime.datetime(datetime.datetime.now().date().year, datetime.datetime.now().date().month,
-                        datetime.datetime.now().date().day, datetime.datetime.now().time().hour,
-                        datetime.datetime.now().time().minute)
+# find current google play store version of dolphin emulator
+googleplaydolphindata = app(
+    'org.dolphinemu.dolphinemu'
 )
+current_android_version = googleplaydolphindata["version"]
+with open('old_android_version_file') as versionfile:
+    old_android_version = versionfile.readlines()
+if old_android_version[0] != current_android_version:
+    #set up beautifulsoup for dolphin emulator download page
+    downloadpage = requests.get("https://dolphin-emu.org/download/")
+    downloadpagesoup = BeautifulSoup(downloadpage.content, 'html.parser')
 
-feed = Feed(
-        title = "Fdroid Update Reminders",
-        link = "https://www.cyberciti.biz/atom/updated.xml",
-        description = "Reminders to update different Fdroid packages",
-        language = "en-US",
-        lastBuildDate = datetime.datetime.now(),
-        items = [item1])
+    # find the download link on the page (using current android version!)
+    try:
+        downloadpagetag = str(downloadpagesoup.body.find('a', string=current_android_version))
+        for letternum in range(len(str(downloadpagetag)[9:])):
+            if str(downloadpagetag)[9:][letternum] == '"':
+                partdownloadlink = str(downloadpagetag)[9:int(9+letternum)]
+                fulldownloadlink = str("https://dolphin-emu.org" + partdownloadlink)
+                break
 
-print(feed.rss())
+        #set up beautifulsoup for commitpage
+        commitpage = requests.get(fulldownloadlink)
+        commitpagesoup = BeautifulSoup(commitpage.content, 'html.parser')
 
-with open('Fdroid_Alerter.xml', 'w') as rssfile:
-    rssfile.write(feed.rss())
+        # find the commit link
+        commitpagetag = commitpagesoup.body.find('dd', "commithash")
+        for letternum in range(len(str(commitpagetag)[32:])):
+            if str(commitpagetag)[32:][letternum] == '"':
+                commitlink = str(commitpagetag)[32:int(32+letternum)]
+                break
+
+        global line1
+        line1 = str("@licaon-kter The current version of Dolphin Emulator on Fdroid is outdated.")
+        global line2
+        line2 = str("The commit link is: " + commitlink)
+        global line3
+        line3 = str("The direct download link is: " + fulldownloadlink)
+        item1 = Item(
+            title = "NEW UPDATE TO DOLPHIN EMULATOR",
+            link = "https://gitlab.com/fdroid/fdroiddata/-/issues/2043",
+            description = '<p>'+ line1 +'</p><p>'+line2+'</p><p>'+line3+'</p>',
+            pubDate = datetime.datetime(datetime.datetime.now().date().year, datetime.datetime.now().date().month,
+                                datetime.datetime.now().date().day, datetime.datetime.now().time().hour,
+                                datetime.datetime.now().time().minute)
+        )
+
+        feed = Feed(
+                title = "Fdroid Update Reminders",
+                link = "https://raw.githubusercontent.com/exaCORE42/FdroidAlerter/master/Fdroid_Alerter.xml?token=AUKAXQEZBLHG46XVVM5465LBY7DDE",
+                description = "Reminders to update different Fdroid packages",
+                language = "en-US",
+                lastBuildDate = datetime.datetime.now(),
+                items = [item1])
 
 
+        with open('Fdroid_Alerter.xml', 'w') as rssfile:
+            rssfile.write(feed.rss())
+    finally:
+        item1 = Item(
+            title="NEW UPDATE TO DOLPHIN EMULATOR",
+            link="https://gitlab.com/fdroid/fdroiddata/-/issues/2043",
+            description='SOMETHING WENT WRONG IN THE CODE, BUT THERE IS STILL A NEW VERSION!  PLEASE CHECK IT OUT!!'
+                        'OLD VERSION: ' + str(old_android_version[0]) + 'NEW VERSION: ' + str(current_android_version)
+            + 'https://dolphin-emu.org/download/ https://play.google.com/store/apps/details?id=org.dolphinemu.dolphinemu&hl=en_US&gl=US',
+            pubDate=datetime.datetime(datetime.datetime.now().date().year, datetime.datetime.now().date().month,
+                                      datetime.datetime.now().date().day, datetime.datetime.now().time().hour,
+                                      datetime.datetime.now().time().minute)
+        )
+        feed = Feed(
+            title="Fdroid Update Reminders",
+            link="https://raw.githubusercontent.com/exaCORE42/FdroidAlerter/master/Fdroid_Alerter.xml?token=AUKAXQEZBLHG46XVVM5465LBY7DDE",
+            description="Reminders to update different Fdroid packages",
+            language="en-US",
+            lastBuildDate=datetime.datetime.now(),
+            items=[item1])
+
+        with open('Fdroid_Alerter.xml', 'w') as rssfile:
+            rssfile.write(feed.rss())
